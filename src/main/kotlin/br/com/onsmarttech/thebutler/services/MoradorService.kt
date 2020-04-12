f93c8e7763d98aedebad28501e148218ce6ab758
Elias Braz <elias.brazjunior@gmail.com>
2020-04-12 00:34:02 -0300

    Listando fichas por morador

---
+++ src/main/kotlin/br/com/onsmarttech/thebutler/services/MoradorService.kt
@@ -0,0 +1,20 @@
+package br.com.onsmarttech.thebutler.services
+
+import br.com.onsmarttech.thebutler.documents.Morador
+import br.com.onsmarttech.thebutler.exception.BadRequestException
+import br.com.onsmarttech.thebutler.repositories.MoradorRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class MoradorService {
+
+    @Autowired
+    private lateinit var moradorRepository: MoradorRepository
+
+    fun saveAll(moradores: List<Morador>) = moradorRepository.saveAll(moradores)
+
+    fun findById(moradorId: String) = moradorRepository.findById(moradorId)
+            .orElseThrow { BadRequestException("Morador não encontrado") }
+
+}
