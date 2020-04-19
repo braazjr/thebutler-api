6ec0c035754c77ffaee1b26853c291513a740aff
Elias Braz <elias.brazjunior@gmail.com>
2020-04-19 15:56:26 -0300

    Listando permissoes

---
+++ src/main/kotlin/br/com/onsmarttech/thebutler/controllers/PermissaoController.kt
@@ -0,0 +1,14 @@
+package br.com.onsmarttech.thebutler.controllers
+
+import br.com.onsmarttech.thebutler.enums.Permissao
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/permissoes")
+class PermissaoController {
+
+    @GetMapping
+    fun list() = Permissao.values()
+}
