89552be811bd57c434c497a33168634c92584d0d
Elias Braz <elias.brazjunior@gmail.com>
2020-05-14 23:32:34 -0300

    Id como opção ao salvar uma ficha

--- src/main/kotlin/br/com/onsmarttech/thebutler/dtos/FichaDto.kt
+++ src/main/kotlin/br/com/onsmarttech/thebutler/dtos/FichaDto.kt
@@ -9,7 +9,7 @@ import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
 data class FichaDto(
-        val id: String,
+        val id: String?,
         @get:NotBlank val idApartamento: String,
         @get:Valid @get:NotEmpty val moradores: List<Morador>,
         @get:NotNull @JsonFormat(pattern = "dd/MM/yyyy") val dataInicio: LocalDate
