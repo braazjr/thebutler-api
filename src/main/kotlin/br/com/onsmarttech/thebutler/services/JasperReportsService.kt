767b088fb693e109eda4dadd71621548aa4c5f98
Elias Braz <elias.brazjunior@gmail.com>
2020-10-13 21:07:13 -0300

    Corrigindo path do subrelatório

--- src/main/kotlin/br/com/onsmarttech/thebutler/services/JasperReportsService.kt
+++ src/main/kotlin/br/com/onsmarttech/thebutler/services/JasperReportsService.kt
@@ -5,6 +5,7 @@ import com.google.gson.Gson
 import net.sf.jasperreports.engine.JasperExportManager
 import net.sf.jasperreports.engine.JasperFillManager
 import net.sf.jasperreports.engine.data.JsonDataSource
+import net.sf.jasperreports.engine.util.JRLoader
 import org.springframework.stereotype.Service
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
@@ -14,12 +15,12 @@ class JasperReportsService {
 
     fun fichaGenerate(ficha: FichaJasperDto): ByteArray {
         val inputStream = this.javaClass.getResourceAsStream("/reports/FichaApartamento.jasper")
-        val subReportPath = this.javaClass.getResource("/reports/FichaMoradores.jasper").path
+        val subReport = this.javaClass.getResourceAsStream("/reports/FichaMoradores.jasper")
 
         val json = Gson().toJson(ficha)
         val dataSource = JsonDataSource(ByteArrayInputStream(json.toByteArray()))
 
-        val paramters = mutableMapOf("SUBREPORT_PATH" to "./FichaMoradores.jasper")
+        val paramters = mutableMapOf("SUBREPORT_PATH" to JRLoader.loadObject(subReport))
 
         val print = JasperFillManager.fillReport(inputStream, paramters as Map<String, Any>?, dataSource)
         val bos = ByteArrayOutputStream()
