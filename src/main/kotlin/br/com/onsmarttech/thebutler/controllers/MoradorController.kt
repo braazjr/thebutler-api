9a2287cbc6cef3d05365263dfddfbbc114b0f42a
Elias Braz <elias.brazjunior@gmail.com>
2020-11-03 18:49:09 -0300

    Pesquisando moradores por id

--- src/main/kotlin/br/com/onsmarttech/thebutler/controllers/MoradorController.kt
+++ src/main/kotlin/br/com/onsmarttech/thebutler/controllers/MoradorController.kt
@@ -5,6 +5,7 @@ import br.com.onsmarttech.thebutler.services.MoradorService
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,4 +18,7 @@ class MoradorController(val moradorService: MoradorService) {
 
     @GetMapping()
     fun findAll(pageable: Pageable, filter: MoradorFilter) = ResponseEntity.ok(moradorService.find(pageable, filter))
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable("id") id: String) = ResponseEntity.ok(moradorService.findById(id))
 }
